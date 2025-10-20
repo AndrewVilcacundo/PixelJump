904a1e73ace91e5b1db0ef0f5ae6144d779e149a
Andrew Vilcacundo <andrewmateo15@hotmail.com>
2025-10-20 15:09:55 -0500

    Primera versión del proyecto Pixel Jump

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("¡Nivel completado!");
+            // Aquí puedes cambiar de escena o mostrar un mensaje
+        }
+    }
+}
